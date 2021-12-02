50a5c62160594b91014f979e6804ff20e372888b
Ray Zhang <raybot@students.cs.ubc.ca>
2021-12-01 21:16:17 -0800

    Day 2

---
+++ Day 2/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func part1(v []string) {
+	h := 0
+	d := 0
+	for i := 0; i < len(v); i++ {
+		cmd := strings.Split(v[i], " ")
+		if cmd[0] == "forward" {
+			v, _ := strconv.Atoi(cmd[1])
+			h += v
+		}
+		if cmd[0] == "backward" {
+			v, _ := strconv.Atoi(cmd[1])
+			h -= v
+		}
+		if cmd[0] == "up" {
+			v, _ := strconv.Atoi(cmd[1])
+			d -= v
+		}
+		if cmd[0] == "down" {
+			v, _ := strconv.Atoi(cmd[1])
+			d += v
+		}
+	}
+	fmt.Println(d * h)
+}
+
+func part2(v []string) {
+
+	h := 0
+	d := 0
+	aim := 0
+	for i := 0; i < len(v); i++ {
+		cmd := strings.Split(v[i], " ")
+		if cmd[0] == "forward" {
+			v, _ := strconv.Atoi(cmd[1])
+			h += v
+			d += v * aim
+		}
+		if cmd[0] == "backward" {
+			v, _ := strconv.Atoi(cmd[1])
+			h -= v
+			d -= v * aim
+		}
+		if cmd[0] == "up" {
+			v, _ := strconv.Atoi(cmd[1])
+			//d -= v
+			aim -= v
+		}
+		if cmd[0] == "down" {
+			v, _ := strconv.Atoi(cmd[1])
+			//d += v
+			aim += v
+		}
+	}
+	fmt.Println(d * h)
+}
+
+func main() {
+
+	content, err := ioutil.ReadFile("Day 2/input.txt")
+	strcontent := string(content)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	v := strings.Split(strcontent, "\n")
+	part1(v)
+	part2(v)
+}
