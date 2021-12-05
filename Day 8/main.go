3635bdfaa59794490e12b1451426459aad38e326
Ray Zhang <raybot@students.cs.ubc.ca>
2021-12-04 21:33:08 -0800

    Templates, days 6-9

---
+++ Day 8/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func part1(v []string) {
+
+	res := 0
+	for i := 0; i < len(v); i++ {
+
+	}
+	fmt.Println(res)
+}
+
+func part2(v []string) {
+
+	res := 0
+	for i := 0; i < len(v); i++ {
+
+	}
+	fmt.Println(res)
+}
+
+func main() {
+
+	content, err := ioutil.ReadFile("Day 8/input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	strcontent := string(content)
+	v := strings.Split(strcontent, "\n")
+	part1(v)
+	part2(v)
+}
