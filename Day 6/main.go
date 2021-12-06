0c8d394e910f3598487a8c213ed2e5e9f7845073
Ray Zhang <raybot@students.cs.ubc.ca>
2021-12-06 12:44:31 -0800

    Oh I forgot to commit day 6 after yesterday

--- Day 6/main.go
+++ Day 6/main.go
@@ -4,14 +4,31 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
 func part1(v []string) {
 
 	res := 0
+	//fish_at_timer := make(map[int]int)
+	fish_at_timer := make([]int, 9)
 	for i := 0; i < len(v); i++ {
+		fish, _ := strconv.Atoi(v[i])
+		fish_at_timer[fish]++
+	}
 
+	for i := 0; i < 80; i++ {
+		newfish := fish_at_timer[0]
+		for j := 0; j < 8; j++ {
+			fish_at_timer[j] = fish_at_timer[j+1]
+		}
+		fish_at_timer[8] = newfish
+		fish_at_timer[6] += newfish
+	}
+
+	for i := 0; i < 9; i++ {
+		res += fish_at_timer[i]
 	}
 	fmt.Println(res)
 }
@@ -19,8 +36,24 @@ func part1(v []string) {
 func part2(v []string) {
 
 	res := 0
+	//fish_at_timer := make(map[int]int)
+	fish_at_timer := make([]int, 9)
 	for i := 0; i < len(v); i++ {
+		fish, _ := strconv.Atoi(v[i])
+		fish_at_timer[fish]++
+	}
 
+	for i := 0; i < 256; i++ {
+		newfish := fish_at_timer[0]
+		for j := 0; j < 8; j++ {
+			fish_at_timer[j] = fish_at_timer[j+1]
+		}
+		fish_at_timer[8] = newfish
+		fish_at_timer[6] += newfish
+	}
+
+	for i := 0; i < 9; i++ {
+		res += fish_at_timer[i]
 	}
 	fmt.Println(res)
 }
@@ -33,7 +66,7 @@ func main() {
 		log.Fatal(err)
 	}
 	strcontent := string(content)
-	v := strings.Split(strcontent, "\n")
+	v := strings.Split(strcontent, ",")
 	part1(v)
 	part2(v)
 }
