dedce2ebb5e4a4d5a26d82c5b1631f886a18cd34
Ray Zhang <raybot@students.cs.ubc.ca>
2021-12-10 22:36:18 -0800

    Oh right once again forgetting to actually commit after celebrating (Day 11)
    Beat my ACM coach though

--- Day 11/main.go
+++ Day 11/main.go
@@ -4,22 +4,110 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
-func part1(v []string) {
+func inbounds(arr [][]int, x int, y int) bool {
+	width := len(arr[0])
+	height := len(arr)
 
+	return x >= 0 && x < width && y >= 0 && y < height
+}
+
+func part1(v []string) {
 	res := 0
+	cavemap := make([][]int, len(v))
 	for i := 0; i < len(v); i++ {
+		line := make([]int, len(v[i]))
+		for j := 0; j < len(v[i]); j++ {
+			point, _ := strconv.Atoi(string(v[i][j]))
+			line[j] = point
+		}
+		cavemap[i] = line
+	}
+
+	for day := 1; day <= 100; day++ {
+		for i := 0; i < len(cavemap); i++ {
+			for j := 0; j < len(cavemap[i]); j++ {
+				cavemap[i][j]++
+			}
+		}
+		flashed := true
+		for flashed {
+			flashed = false
+			for i := 0; i < len(cavemap); i++ {
+				for j := 0; j < len(cavemap[i]); j++ {
+					if cavemap[i][j] > 9 {
+						flashed = true
+						res++
+						cavemap[i][j] = 0
+						for k := -1; k <= 1; k++ {
+							for l := -1; l <= 1; l++ {
+								if inbounds(cavemap, j+k, i+l) && cavemap[i+l][j+k] > 0 {
+									cavemap[i+l][j+k]++
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 
 	}
 	fmt.Println(res)
 }
 
 func part2(v []string) {
-
 	res := 0
+	cavemap := make([][]int, len(v))
 	for i := 0; i < len(v); i++ {
+		line := make([]int, len(v[i]))
+		for j := 0; j < len(v[i]); j++ {
+			point, _ := strconv.Atoi(string(v[i][j]))
+			line[j] = point
+		}
+		cavemap[i] = line
+	}
+
+	for day := 1; ; day++ {
+		for i := 0; i < len(cavemap); i++ {
+			for j := 0; j < len(cavemap[i]); j++ {
+				cavemap[i][j]++
+			}
+		}
+		flashed := true
+		for flashed {
+			flashed = false
+			for i := 0; i < len(cavemap); i++ {
+				for j := 0; j < len(cavemap[i]); j++ {
+					if cavemap[i][j] > 9 {
+						flashed = true
+						cavemap[i][j] = 0
+						for k := -1; k <= 1; k++ {
+							for l := -1; l <= 1; l++ {
+								if inbounds(cavemap, j+k, i+l) && cavemap[i+l][j+k] > 0 {
+									cavemap[i+l][j+k]++
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		synced := true
+		for i := 0; i < len(cavemap); i++ {
+			for j := 0; j < len(cavemap[i]); j++ {
+				if cavemap[i][j] > 0 {
+					synced = false
+				}
+			}
+		}
+		if synced {
+			res = day
+			break
+		}
 
 	}
 	fmt.Println(res)
